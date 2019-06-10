fb42a6abc705c349806d8fb57d917d04fcb05cd5
shar09 <sharathtelu9@gmail.com>
2019-06-10 14:27:10 -0500

    console log articles done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
-var awesomness = ["Alex", "Andrew", "Anton", "Shar"]
+var awesomness = ["Alex", "Andrew", "Anton", "Shar"];
+var searchTitle = "";
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTitle+"&api-key=wcVYQYtAoquqNwS0WW2mmdlVGA7qgDGV";
+
+function displayArticle() {
+$.ajax({
+    url: queryUrl,
+    method: "GET"
+}).then(function(response) {
+   console.log(response);
+   for(var i=0;i<10;i++) {
+   console.log(response.response.docs[i].headline.main);
+   }
+});
+}
+$("#search").on("click", function(event){
+    event.preventDefault();
+    var searchCurrent = $("#search-term").val().trim();
+    console.log(searchCurrent);
+    var searchTitle = searchCurrent;
+    displayArticle();
+});
