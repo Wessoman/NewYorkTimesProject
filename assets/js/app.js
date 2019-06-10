d4d6835dd7242395deaf51a21a6b15ba91b7e948
Andrew Griswold <andrewgriswold7@gmail.com>
2019-06-10 14:24:04 -0500

    Added function to get articles (not working)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
-var awesomness = ["Alex", "Andrew", "Anton", "Shar"]
+var searchTitle = $('#search-term');
+var searchNumb = 5;
+
+function getArticles() {
+    console.log(what);
+    
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTitle + "&api-key=wcVYQYtAoquqNwS0WW2mmdlVGA7qgDGV";
+    
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(response){
+        console.log(response);
+        for (var i = 0; i < searchNumb; i++) {
+            var articleDIV = $('<div>')
+            var article = $('<p>').text(response)
+            articleDIV.append(article);
+        }
+})
+};
+
+
+
+
+
+
+
+
